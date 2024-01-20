57ef3cbe5cb8624648397163f90f081c3d51eaa2
ChirujanoCodding <trdfabris@gmail.com>
2024-01-20 18:04:50 -0300

    chore(back): :wrench: updated config of nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+            protocol: 'http',
+            hostname: '**'
+        },
+        {
+            protocol: 'https',
+            hostname: '**'
+        }]
+    }
+}
 
 module.exports = nextConfig
