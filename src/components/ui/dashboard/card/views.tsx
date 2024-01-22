32767bd56b913fc5919b14d4a4b31133d2557096
ChirujanoCodding <trdfabris@gmail.com>
2024-01-21 23:31:18 -0300

    style: :lipstick: center children

--- src/components/ui/dashboard/card/views.tsx
+++ src/components/ui/dashboard/card/views.tsx
@@ -2,7 +2,7 @@ import { Eye } from 'lucide-react';
 
 export function Views({ content }: { content: number }) {
 	return (
-		<p className='flex font-semibold text-sm gap-x-2'>
+		<p className='flex font-semibold text-sm gap-x-2 items-center'>
 			<Eye />
 			<span className=' min-w-10 text-end'>{content}</span>
 		</p>
