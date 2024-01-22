c7f6df6bb60245db832a0d47f1b05823d77de3d1
ChirujanoCodding <trdfabris@gmail.com>
2024-01-21 23:37:53 -0300

    feat: :heavy_plus_sign: add tailwind-scrollbar-hide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,7 +87,7 @@ const config = {
 			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
+	plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
 } satisfies Config;
 
 export default config;
