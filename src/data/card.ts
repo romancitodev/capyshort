60ec886551af06dfeb8df67906446f1c15281827
ChirujanoCodding <trdfabris@gmail.com>
2024-01-27 16:13:46 -0300

    fix: :pencil2: rename `getCards` to `getLinks`

--- src/data/card.ts
+++ src/data/card.ts
@@ -1,5 +1,5 @@
 import { db } from '@/lib/db';
 
-export async function getCardsByUser(userId: string) {
+export async function getLinksByUser(userId: string) {
 	return await db.link.findMany({ where: { userId } });
 }
