61f5d1f7ff3fa346b317a93bd8d07156a93f9930
ChirujanoCodding <trdfabris@gmail.com>
2024-02-02 18:51:30 -0300

    fix: :ambulance: critical fix in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,36 +13,33 @@ const { auth } = NextAuth(authConfig);
 export default auth(async req => {
 	const { nextUrl } = req;
 
-	console.log(nextUrl);
-
-	/// css stuff
+	// Serve static files from /_next/ directory
 	if (nextUrl.pathname.startsWith('/_next/')) {
 		return NextResponse.next();
 	}
 
 	const isLogged = !!req.auth;
-
 	const onApiRoute = nextUrl.pathname.startsWith(authPrefix);
 	const onPublicRoute = publicRoutes.includes(nextUrl.pathname);
 	const onAuthRoute = authRoutes.includes(nextUrl.pathname);
 	const onProtectedRoute = protectedRoutes.includes(nextUrl.pathname);
 
-	console.log({
-		isLogged,
-		onApiRoute,
-		onPublicRoute,
-		onAuthRoute,
-		onProtectedRoute,
-	});
+	// Public and API routes don't require authentication
+	if (onApiRoute || onPublicRoute) {
+		return NextResponse.next();
+	}
 
-	if (onApiRoute) return null;
-	if (onPublicRoute) return null;
-
-	if (onAuthRoute && isLogged)
+	// Authenticated users shouldn't reach auth routes
+	if (onAuthRoute && isLogged) {
 		return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+	}
 
-	if (onAuthRoute && !isLogged) return null;
+	// Unauthenticated users should be redirected to login page for protected routes
+	if (!isLogged && onProtectedRoute) {
+		return Response.redirect(new URL('/login', nextUrl));
+	}
 
+	// For other paths, treat them as links and attempt to find the corresponding URL
 	if (!onPublicRoute && !onProtectedRoute) {
 		try {
 			const url = nextUrl.pathname.slice(1, nextUrl.pathname.length);
@@ -51,8 +48,6 @@ export default auth(async req => {
 					? `http://localhost:3000/api/link?code=${url}`
 					: `${process.env.DOMAIN}/api/link?code=${url}`;
 
-			console.log({ urlToFetch });
-
 			const foundedUrl: { link: string | null } = await (
 				await fetch(urlToFetch)
 			).json();
@@ -60,17 +55,15 @@ export default auth(async req => {
 			if (foundedUrl.link) {
 				return NextResponse.redirect(new URL(foundedUrl.link));
 			}
+
+			return Response.redirect(new URL('/', nextUrl));
 		} catch (error) {
 			console.log(error);
 			return Response.redirect(new URL('/', nextUrl));
 		}
 	}
 
-	if (!isLogged && !onPublicRoute)
-		return Response.redirect(new URL('/login', nextUrl));
-
-	if (onPublicRoute || onProtectedRoute) return null;
-
+	// Default case, if none of the above conditions met, just proceed to the next middleware
 	return null;
 });
 
